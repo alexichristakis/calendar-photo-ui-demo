6c1c765492a802b61f3fec07da6be4dcdcd02d28
Alexi Christakis <alexi.christakis@gmail.com>
2019-07-04 14:20:43 -0400

    working drag to dismiss

--- src/components/Transitioner.tsx
+++ src/components/Transitioner.tsx
@@ -6,7 +6,7 @@ import Interactable from "react-native-interactable";
 import { ReduxStateType } from "state";
 import { focusOff } from "state/app";
 import { Colors } from "lib/styles";
-import { SCREEN_HEIGHT, SCREEN_WIDTH, SETTINGS_WIDTH } from "lib/constants";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "lib/constants";
 
 interface TransitionerProps {
   x: number;
@@ -16,150 +16,176 @@ interface TransitionerProps {
   focusOff: () => {};
 }
 
-const ANIMATION_DURATION = 1500;
+const ANIMATION_DURATION = 250;
 
 class Transitioner extends PureComponent<TransitionerProps> {
+  state = {
+    open: false,
+    transitioning: false
+  };
+
   transitionAmount = new Animated.Value(0);
-  reactiveX = new Animated.Value(0);
-  reactiveY = new Animated.Value(0);
+  panAmount = new Animated.ValueXY({ x: 0, y: 0 });
+  _panAmountValue = { x: 0, y: 0 };
+  panListener = "";
 
   componentDidMount() {
-    this.reactiveX.addListener(this.handleInteractablePan);
-    // this.reactiveY.addListener(this.handleInteractablePan);
+    this.panListener = this.panAmount.addListener(this.handleInteractablePan);
   }
 
   componentWillUnmount() {
-    // this.reactiveX.removeAllListeners();
-    // this.reactiveY.removeAllListeners();
+    this.panAmount.removeListener(this.panListener);
   }
 
   componentDidUpdate(prevProps, prevState) {
+    const { transitioning, open } = this.state;
     const { visible } = this.props;
-    if (visible) {
+    if (visible && !open && !transitioning) {
       this.open();
     }
   }
 
-  handleInteractablePan = ({ value }: { value: number }) => {
-    console.log(value);
-    if (Math.abs(value) > 100) {
+  handleInteractablePan = ({ x, y }: { x: number; y: number }) => {
+    // update value
+    this._panAmountValue = { x, y };
+
+    const { transitioning, open } = this.state;
+    if ((Math.abs(x) > 100 || Math.abs(y) > 100) && open && !transitioning) {
       this.close();
     }
   };
 
   open = () => {
-    // Animated.timing(this.transitionAmount, {
-    //   useNativeDriver: true,
-    //   toValue: 1,
-    //   duration: ANIMATION_DURATION,
-    //   easing: Easing.ease
-    // }).start();
-    this.interactable.snapTo({ index: 1 });
+    this.setState({ transitioning: true }, () => {
+      Animated.timing(this.transitionAmount, {
+        useNativeDriver: true,
+        toValue: 1,
+        duration: ANIMATION_DURATION,
+        easing: Easing.ease
+      }).start(() => this.setState({ transitioning: false, open: true }));
+    });
+
+    // this.interactable.snapTo({ index: 1 });
   };
 
   close = () => {
     const { focusOff } = this.props;
-    this.interactable.snapTo({ index: 0 });
-    // this.reactiveX.setValue(0);
-    // this.reactiveY.setValue(0);
-    // Animated.timing(this.transitionAmount, {
-    //   useNativeDriver: true,
-    //   toValue: 0,
-    //   duration: ANIMATION_DURATION,
-    //   easing: Easing.out(Easing.ease)
-    // }).start(() => focusOff());
+    const { x, y } = this._panAmountValue;
+
+    const currentProgress = x / SCREEN_WIDTH + y / SCREEN_HEIGHT;
+
+    this.setState({ transitioning: true }, () => {
+      this.transitionAmount.setValue(1 - currentProgress);
+      Animated.timing(this.transitionAmount, {
+        useNativeDriver: true,
+        toValue: 0,
+        duration: ANIMATION_DURATION,
+        easing: Easing.out(Easing.ease)
+      }).start(() => {
+        focusOff();
+        this.setState({ transitioning: false, open: false });
+      });
+    });
   };
 
   handleOnSnap = ({ nativeEvent: { index } }: Interactable.ISnapEvent) => {
-    const { focusOff } = this.props;
-    if (!index) {
-      focusOff();
-    }
+    console.log("snapping:", index);
+    // const { focusOff } = this.props;
+    // if (!index) {
+    //   focusOff();
+    // }
   };
 
   render() {
+    const { transitioning, open } = this.state;
     const { visible, element, x, y } = this.props;
 
     const animatedScale = {
       transform: [
         {
-          scale: Animated.add(
-            Animated.divide(this.reactiveX, SCREEN_WIDTH),
-            Animated.divide(this.reactiveY, SCREEN_HEIGHT)
-          ).interpolate({
-            inputRange: [0, 1, 2],
-            outputRange: [50 / (SCREEN_WIDTH - 20), 1, 50 / (SCREEN_WIDTH - 20)],
-            extrapolate: "clamp"
-          })
-          // scale: Animated.add(
-          //   this.transitionAmount,
-          //   Animated.add(
-          //     Animated.divide(this.reactiveX, SCREEN_WIDTH),
-          //     Animated.divide(this.reactiveY, SCREEN_HEIGHT)
-          //   )
-          // ).interpolate({
-          //   inputRange: [0, 1, 2],
-          //   outputRange: [50 / (SCREEN_WIDTH - 20), 1, 50 / (SCREEN_WIDTH - 20)],
-          //   extrapolate: "clamp"
-          // })
+          scale: transitioning
+            ? this.transitionAmount.interpolate({
+                inputRange: [0, 1],
+                outputRange: [50 / (SCREEN_WIDTH - 20), 1],
+                extrapolate: "clamp"
+              })
+            : Animated.add(
+                Animated.divide(this.panAmount.x, SCREEN_WIDTH),
+                Animated.divide(this.panAmount.y, SCREEN_HEIGHT)
+              ).interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 50 / (SCREEN_WIDTH - 20)],
+                extrapolate: "clamp"
+              })
         }
       ]
     };
 
     const animatedTranslate = {
+      transform: transitioning
+        ? [
+            {
+              translateY: this.transitionAmount.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-1 * (SCREEN_HEIGHT / 2) + y + 30, 0]
+              })
+            },
+            {
+              translateX: this.transitionAmount.interpolate({
+                inputRange: [0, 1],
+                outputRange: [x - SCREEN_WIDTH / 2 + 30, 0]
+              })
+            }
+          ]
+        : [
+            {
+              translateY: Animated.divide(this.panAmount.y, SCREEN_HEIGHT).interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -1 * (SCREEN_HEIGHT / 2) + y + 30]
+              })
+            },
+            {
+              translateX: Animated.divide(this.panAmount.x, SCREEN_WIDTH).interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, x - SCREEN_WIDTH / 2 + 30]
+              })
+            }
+          ]
+    };
+
+    const animatedBackground = {
+      opacity:
+        transitioning || !open
+          ? this.transitionAmount.interpolate({
+              inputRange: [0, 1],
+              outputRange: [0, 0.8]
+            })
+          : Animated.add(
+              Animated.divide(this.panAmount.x, SCREEN_WIDTH),
+              Animated.divide(this.panAmount.y, SCREEN_HEIGHT)
+            ).interpolate({
+              inputRange: [0, 1],
+              outputRange: [0.8, 0]
+            })
+    };
+
+    const reactive = {
       transform: [
         {
-          translateY: Animated.add(
-            this.transitionAmount,
-            Animated.divide(this.reactiveY, SCREEN_HEIGHT)
-          ).interpolate({
-            inputRange: [0, 1],
-            outputRange: [-1 * (SCREEN_HEIGHT / 2) + y + 30, 0]
+          translateX: this.panAmount.x.interpolate({
+            inputRange: [-100, 0, 100],
+            outputRange: [100, 0, -100]
           })
         },
         {
-          translateX: Animated.add(
-            this.transitionAmount,
-            Animated.divide(this.reactiveX, SCREEN_WIDTH)
-          ).interpolate({
-            inputRange: [0, 1],
-            outputRange: [x - SCREEN_WIDTH / 2 + 30, 0]
+          translateY: this.panAmount.y.interpolate({
+            inputRange: [-100, 0, 100],
+            outputRange: [100, 0, -100]
           })
         }
       ]
     };
 
-    const animatedBackground = {
-      opacity: Animated.add(
-        this.transitionAmount,
-        Animated.add(
-          Animated.divide(this.reactiveX, SCREEN_WIDTH),
-          Animated.divide(this.reactiveY, SCREEN_HEIGHT)
-        )
-      ).interpolate({
-        inputRange: [0, 1, 2],
-        outputRange: [0, 0.8, 0]
-      })
-    };
-
-    const reactive = {
-      // transform: [
-      //   {
-      //     translateX: this.reactiveX.interpolate({
-      //       inputRange: [-100, 0, 100],
-      //       outputRange: [100, 0, -100]
-      //     })
-      //   },
-      //   {
-      //     translateY: this.reactiveY.interpolate({
-      //       inputRange: [-100, 0, 100],
-      //       outputRange: [100, 0, -100]
-      //     })
-      //   }
-      // ]
-    };
-
-    // if (visible)
     return (
       <View style={styles.container} pointerEvents={visible ? "box-none" : "none"}>
         <Animated.View style={[animatedBackground, styles.background]}>
@@ -167,21 +193,19 @@ class Transitioner extends PureComponent<TransitionerProps> {
         </Animated.View>
         <Interactable.View
           animatedNativeDriver
-          ref={interactable => (this.interactable = interactable)}
-          snapPoints={[{ x, y }, { x: 0, y: 0 }]}
+          dragEnabled={!transitioning}
+          snapPoints={[{ x: 0, y: 0, damping: 0.5, tension: 500 }]}
           onSnap={this.handleOnSnap}
-          initialPosition={{ x, y }}
-          style={[animatedTranslate, reactive]}
-          animatedValueX={this.reactiveX}
-          animatedValueY={this.reactiveY}
+          style={reactive}
+          animatedValueX={this.panAmount.x}
+          animatedValueY={this.panAmount.y}
         >
-          {/* <Animated.View style={animatedTranslate}> */}
-          <Animated.View style={animatedScale}>{element}</Animated.View>
-          {/* </Animated.View> */}
+          <Animated.View style={animatedTranslate}>
+            <Animated.View style={animatedScale}>{visible ? element : null}</Animated.View>
+          </Animated.View>
         </Interactable.View>
       </View>
     );
-    // else return null;
   }
 }
 
